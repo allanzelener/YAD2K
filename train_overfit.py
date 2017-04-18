d4f632bd81b3698ad0e89c3c2af5c6b1b71caaa8
Allan Zelener <allanzelener@gmail.com>
2017-04-18 05:08:33 -0400

    Remove cfgs and anchor, move and revise sample train script.

--- train_overfit.py
+++ train_overfit.py
@@ -1,3 +1,8 @@
+"""Overfit a YOLO_v2 model to a single image from the Pascal VOC dataset.
+
+This is a sample training script used to test the implementation of the
+YOLO localization loss function.
+"""
 import io
 import os
 
@@ -8,7 +13,7 @@ import PIL
 import tensorflow as tf
 from keras import backend as K
 from keras.layers import Input, Lambda
-from keras.models import Model, load_model
+from keras.models import Model
 
 from yad2k.models.keras_yolo import (preprocess_true_boxes, yolo_body,
                                      yolo_eval, yolo_head, yolo_loss)
@@ -19,9 +24,10 @@ COCO_ANCHORS = np.array(
      (10.246, 4.59428), (12.6868, 11.8741)))
 
 
-def main():
+def _main():
     voc_path = os.path.expanduser('~/datasets/VOCdevkit/pascal_voc_07_12.hdf5')
     classes_path = os.path.expanduser('model_data/pascal_classes.txt')
+
     with open(classes_path) as f:
         class_names = f.readlines()
     class_names = [c.strip() for c in class_names]
@@ -30,76 +36,97 @@ def main():
     image = PIL.Image.open(io.BytesIO(voc['train/images'][28]))
     orig_size = np.array([image.width, image.height])
     orig_size = np.expand_dims(orig_size, axis=0)
+
+    # Image preprocessing.
     image = image.resize((416, 416), PIL.Image.BICUBIC)
     image_data = np.array(image, dtype=np.float)
     image_data /= 255.
+
+    # Box preprocessing.
+    # Original boxes stored as 1D list of class, x_min, y_min, x_max, y_max.
     boxes = voc['train/boxes'][28]
     boxes = boxes.reshape((-1, 5))
+    # Get extents as y_min, x_min, y_max, x_max, class for comparision with
+    # model output.
     boxes_extents = boxes[:, [2, 1, 4, 3, 0]]
+
+    # Get box parameters as x_center, y_center, box_width, box_height, class.
     boxes_xy = 0.5 * (boxes[:, 3:5] + boxes[:, 1:3])
     boxes_wh = boxes[:, 3:5] - boxes[:, 1:3]
     boxes_xy = boxes_xy / orig_size
     boxes_wh = boxes_wh / orig_size
     boxes = np.concatenate((boxes_xy, boxes_wh, boxes[:, 0:1]), axis=1)
-    image_data = np.expand_dims(image_data, axis=0)
+
+    # Precompute detectors_mask and matching_true_boxes for training.
+    # Detectors mask is 1 for each spatial position in the final conv layer and
+    # anchor that should be active for the given boxes and 0 otherwise.
+    # Matching true boxes gives the regression targets for the ground truth box
+    # that caused a detector to be active or 0 otherwise.
+    anchors = COCO_ANCHORS
+    detectors_mask_shape = (13, 13, 5, 1)
+    matching_boxes_shape = (13, 13, 5, 5)
+    detectors_mask, matching_true_boxes = preprocess_true_boxes(boxes, anchors,
+                                                                [416, 416])
+
+    # Create model input layers.
     image_input = Input(shape=(416, 416, 3))
     boxes_input = Input(shape=(None, 5))
-    box_flags_shape = (13, 13, 5, 1)
-    matching_boxes_shape = (13, 13, 5, 5)
-    box_flags_input = Input(shape=box_flags_shape)
+    detectors_mask_input = Input(shape=detectors_mask_shape)
     matching_boxes_input = Input(shape=matching_boxes_shape)
-    anchors = COCO_ANCHORS
-    print(anchors.shape, boxes.shape)
 
-    box_flags, matching_true_boxes = preprocess_true_boxes(boxes, anchors,
-                                                           [416, 416])
     print(boxes)
     print(boxes_extents)
-    print(np.where(box_flags == 1)[:-1])
-    print(matching_true_boxes[np.where(box_flags == 1)[:-1]])
-    # from IPython import embed
-    # embed()
+    print(np.where(detectors_mask == 1)[:-1])
+    print(matching_true_boxes[np.where(detectors_mask == 1)[:-1]])
 
+    # Create model body.
     model_body = yolo_body(image_input, len(anchors), len(class_names))
     model_body = Model(image_input, model_body.output)
+    # Place model loss on CPU to reduce GPU memory usage.
     with tf.device('/cpu:0'):
+        # TODO: Replace Lambda with custom Keras layer for loss.
         model_loss = Lambda(
             yolo_loss,
             output_shape=(1, ),
             name='yolo_loss',
             arguments={'anchors': anchors,
                        'num_classes': len(class_names)})([
-                           model_body.output, boxes_input, box_flags_input,
-                           matching_boxes_input
+                           model_body.output, boxes_input,
+                           detectors_mask_input, matching_boxes_input
                        ])
     model = Model(
-        [image_input, boxes_input, box_flags_input,
+        [image_input, boxes_input, detectors_mask_input,
          matching_boxes_input], model_loss)
     model.compile(
         optimizer='adam', loss={
             'yolo_loss': lambda y_true, y_pred: y_pred
-        })  # This is a hack to use the custom loss function.
+        })  # This is a hack to use the custom loss function in the last layer.
 
+    # Add batch dimension for training.
+    image_data = np.expand_dims(image_data, axis=0)
     boxes = np.expand_dims(boxes, axis=0)
-    box_flags = np.expand_dims(box_flags, axis=0)
+    detectors_mask = np.expand_dims(detectors_mask, axis=0)
     matching_true_boxes = np.expand_dims(matching_true_boxes, axis=0)
 
     num_steps = 10000
+    # TODO: For full training, put preprocessing inside training loop.
     # for i in range(num_steps):
     #     loss = model.train_on_batch(
-    #         [image_data, boxes, box_flags, matching_true_boxes], np.zeros(len(image_data)))
-    #     print(i, loss)
-    model.fit([image_data, boxes, box_flags, matching_true_boxes],
+    #         [image_data, boxes, detectors_mask, matching_true_boxes],
+    #         np.zeros(len(image_data)))
+    model.fit([image_data, boxes, detectors_mask, matching_true_boxes],
               np.zeros(len(image_data)),
               batch_size=1,
               nb_epoch=num_steps)
     model.save_weights('overfit_weights.h5')
 
+    # Create output variables for prediction.
     yolo_outputs = yolo_head(model_body.output, anchors, len(class_names))
     input_image_shape = K.placeholder(shape=(2, ))
     boxes, scores, classes = yolo_eval(
         yolo_outputs, input_image_shape, score_threshold=.3, iou_threshold=.9)
 
+    # Run prediction on overfit image.
     sess = K.get_session()  # TODO: Remove dependence on Tensorflow session.
     out_boxes, out_scores, out_classes = sess.run(
         [boxes, scores, classes],
@@ -110,6 +137,8 @@ def main():
         })
     print('Found {} boxes for image.'.format(len(out_boxes)))
     print(out_boxes)
+
+    # Plot image with predicted boxes.
     image_with_boxes = draw_boxes(image_data[0], out_boxes, out_classes,
                                   class_names, out_scores)
     plt.imshow(image_with_boxes, interpolation='nearest')
@@ -117,4 +146,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    _main()
